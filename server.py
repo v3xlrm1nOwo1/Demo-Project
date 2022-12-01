780d8fec2e5f8c0fbae4d8ba6f72c429431844d7
Black Heart <developerblackheart@gmail.com>
2022-12-01 19:15:29 +0500

    added some code in server.py file

--- server.py
+++ server.py
@@ -5,10 +5,11 @@ import sqlite3
 # addr
 host = "127.0.0.1" # localhost
 port = 1234
+id = 1
 
 con = sqlite3.connect("Demo.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE info(first_name, last_name, addr, academic year,semester,Tel)")
+cur.execute("CREATE TABLE infoh(ID PRIMARY KEY, first_name, last_name, addr, academic, year,semester,Tel)")
 
 
 
@@ -17,10 +18,65 @@ server.bind((host, port))
 server.listen()
 
 
-def searching():
-    pass
+def add_(data):
+    cur.executemany("INSERT INTO infoh VALUES(?, ?, ?, ?, ?, ?, ?, ?)", data)
+    con.commit()
 
 
-def add():
-    pass
+def searching(id):
+    for row in cur.execute(f"SELECT ID, first_name, last_name, addr, academic, year,semester,Tel FROM infoh WHERE ID = {id}"):
+        print(row)
 
+
+def send(data):
+    return str(data).rjust(1024, " ").encode("utf-8")
+
+
+def main():
+    server.listen()
+    conn, addr = server.accept()
+    list_ = []
+    l = ()
+    print("Online.....")
+    send("You can add and search")
+    
+    if dat.decode("utf-8").strip() == "add":
+        send("fname")
+        if type(dat.decode("utf-8").strip()) == str:
+            list_.append(dat.decode("utf-8").strip())
+            
+            send("lname")
+            if type(dat.decode("utf-8").strip()) == str:
+                list_.append(dat.decode("utf-8").strip())
+                
+                l = []
+                send(f"you can addr: {l}")
+                send("addr")
+                if type(dat.decode("utf-8").strip()) == str and dat.decode("utf-8").strip() in l:
+                    list_.append(dat.decode("utf-8").strip())
+                    
+        if len(list_) == 7:
+            l = (id, list_[0], list_[1], list_[2], list_[3], list_[4], list_[5], list_[6])
+            list_ = [l]
+            add_(list_)
+            id += 1
+                
+        else:
+            send("try enter your data...")
+    
+    elif dat.decode("utf-8").strip() == "search":
+        send("id")
+        if searching(dat.decode("utf-8").strip()):
+            send(searching(dat.decode("utf-8").strip()))
+        else:
+            send("")
+    
+    
+    else:
+        send("Error ):")
+        
+    thread = threading.Thread(target=add_, args=(data, ))
+    thread.start()
+        
+    thread = threading.Thread(target=searching, args=(id, ))
+    thread.start()
--- try.py
+++ try.py
@@ -1,10 +1,28 @@
-import sqlite3
+# import sqlite3
 
-con = sqlite3.connect("tutorial.db")
+# con = sqlite3.connect("Demo.db")
+# cur = con.cursor()
+# # cur.execute("CREATE TABLE infoh(ID, first_name, last_name, addr, academic, year,semester,Tel)")
+# #
 
-cur = con.cursor()
+# def add_(data):
+#     cur.executemany("INSERT INTO infoh VALUES(?, ?, ?, ?, ?, ?, ?, ?)", data)
+#     con.commit()
 
-con = sqlite3.connect("Demo.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE info(first_name, last_name, addr, academic year,semester,Tel)")
 
+# def searching(id):
+#     for row in cur.execute(f"SELECT ID, first_name, last_name, addr, academic, year,semester,Tel FROM infoh WHERE ID = {id}"):
+#         print(row)
+
+
+# data = [
+#     (1,'batool','gammer','bahry','nw',2022,6,'0927490519')
+# ]
+
+# add_(data)
+
+# searching(1)
+list_ = [1, 2, 3, 4, 5, 6, 7, 8]
+l = (list_[0], list_[1], list_[2], list_[3], list_[4], list_[5], list_[6], list_[7])
+list_ = [l]
+print(list_)
